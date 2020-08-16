25c61785029a39670fa4b421efb74be52164e2f4
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-08-16 16:44:39 -0500

    backend built - structure, routes, script, models completed

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require("mongoose");
+const routes = require(require("./routes"));
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.use(routes);
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/GoogleBooksList"
+);
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function () {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});

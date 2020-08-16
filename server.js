4aab35acaac137aae5cbb1f2e358fbeea183615e
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-08-16 17:03:53 -0500

    added logger

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
-const routes = require(require("./routes"));
+const routes = require("./routes"));
+const logger = require("morgan";)
 
+app.use(logger("dev"));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
